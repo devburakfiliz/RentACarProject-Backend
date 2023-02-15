33c3450aefa904cefa91b6b60ab930a149576080
Burak Filiz <dev.burakfiliz@gmail.com>
2023-02-15 11:33:20 +0300

    .

---
+++ Core/Utilities/Security/JWT/ITokenHelper.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.JWT
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims); // kullanıcı sıfre ve ıd gırdıgınde  apiye yolladı ilgılı kullanıcı ıcın verı tabanında kullanıcı clıamlarını bulup json web token uretıp onu yollayacak
+
+    }
+}
